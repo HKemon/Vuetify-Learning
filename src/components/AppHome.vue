a9f25d35713bb49296c83a4f4cfbd90adbca9dbb
emon <hk.emon.007@gmail.com>
2018-09-15 18:47:46 +0600

    Header and Footer added

---
+++ src/components/AppHome.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <app-toolbar></app-toolbar>
+        <app-footer></app-footer>
+    </div>
+</template>
+
+<script>
+    import AppFooter from './AppFooter'
+    import AppToolbar from './AppToolbar'
+
+    export default {
+        name: "AppHome",
+        components: {
+            AppFooter, AppToolbar
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppToolbar.vue
@@ -0,0 +1,12 @@
+<template>
+    <v-toolbar color="rgba(255, 0, 0, 0.5)" dark>
+        <v-toolbar-side-icon></v-toolbar-side-icon>
+        <v-toolbar-title>Title</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down">
+            <v-btn flat>Link One</v-btn>
+            <v-btn flat>Link Two</v-btn>
+            <v-btn flat>Link Three</v-btn>
+        </v-toolbar-items>
+    </v-toolbar>
+</template>
